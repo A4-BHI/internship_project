deabb09b1c066ea28d88c71ac96a6d5455c70697
A4-BHI <arpitagari17@gmail.com>
2024-07-28 15:01:19 +0530

    favicon changed by a4.bhi

--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -13,10 +13,7 @@ function HomePage() {
         <div className="wrapper">
           <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            explicabo suscipit cum eius, iure est nulla animi consequatur
-            facilis id pariatur fugit quos laudantium temporibus dolor ea
-            repellat provident impedit!
+          Bhumi is an innovative real estate website designed to simplify the process of buying, selling, and renting properties. With a user-friendly interface, Bhumi offers comprehensive property listings, detailed descriptions, high-quality images, and virtual tours to help users make informed decisions. 
           </p>
           <SearchBar />
           <div className="boxes">
--- socket/app.js
+++ socket/app.js
@@ -38,4 +38,6 @@ io.on("connection", (socket) => {
   });
 });
 
-io.listen("4000");
+io.listen("4000",()=>{
+  console.log("Socket is running")
+});
